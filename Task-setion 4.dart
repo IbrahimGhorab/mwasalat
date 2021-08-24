1d878381b8fd78daede90270cdba2c9ca2fb75a4
IbrahimGhorab <89431527+IbrahimGhorab@users.noreply.github.com>
2021-08-24 12:46:34 +0200

    Add files via upload

---
+++ Task-setion 4.dart
@@ -0,0 +1,43 @@
+main() {
+  Map<int, Map<String, dynamic>> buss = {
+  1 : {
+    'number' : 16,
+    'line' : 'ca, lxr',
+    'stations' : [
+      'cair', 'lxr', 'qouba'
+      ]
+  },
+  2 : {
+    'number' : 16,
+    'line' : 'ca, lxr',
+    'stations' : [
+      'cair', 'lxr', 'qouba'
+      ]
+  },
+  3 : {
+    'number' : 16,
+    'line' : 'ca to lxr',
+    'stations' : [
+      'cair', 'lxr', 'qouba'
+      ]
+  },
+};
+
+// i hope that will be a good answer
+/* 
+for(int x=0;x<buss.length;x++){
+  for(int y=0;y<buss.values.length;y++){
+    print('the value number ${y} in Key number ${x} have the word "to": ${buss.values.elementAt(x).values.elementAt(y).toString().contains("to")}');   
+  }
+}
+*/
+
+
+
+
+
+
+
+
+  
+}
